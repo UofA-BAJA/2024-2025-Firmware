41ba5b0968a0c7b18fa1f664f79fbe4268a6e0fd
karsteny <42128680+karsteny@users.noreply.github.com>
2024-10-23 13:54:03 -0700

    CAN works!

--- tests/New Stuff Test/src/main.cpp
+++ tests/New Stuff Test/src/main.cpp
@@ -9,7 +9,8 @@
 const int CAN_CS_PIN = 10;
 
 // Devices
-HT16K33 display;         // 14 segment
+HT16K33 display; // 14 segment
+HT16K33 display2;
 MCP_CAN CAN(CAN_CS_PIN); // CAN
 Servo speed;
 Servo rpm;
@@ -19,35 +20,43 @@ Servo rpm;
 void setup()
 {
   // <insert that default comment that gets generated with all arduino projects>
-  Serial.begin(9600);
-  Serial.println("Dash test r6");
+  Serial.begin(115200);
+  Serial.println("Dash test r7");
 
   // Join I2C
-  Wire.begin();
+  // Wire.begin();
 
   //---------------------------------------------------
 
   // initalize 14 segment and check for display acknowledge
-  if (display.begin(0x70, 0x71) == false)
-  {
-    Serial.println("At least one 14 segment did not acknowledge! Freezing.");
-    while (1)
-      ;
-  }
+  // if (display.begin(0x70, 0x71) == false)
+  // {
+  //   Serial.println("At least one 14 segment from row 1 did not acknowledge!");
+  //   // while (1)
+  //   //   ;
+  // }
+  // if (display2.begin(0x72, 0x73) == false)
+  // {
+  //   Serial.println("At least one 14 segment from row 2 did not acknowledge!");
+  //   // while (1)
+  //   //   ;
+  // }
   Serial.println("Display acknowledged.");
-  display.print("DISPTEST");
+  Serial.println("lklkljhfhfjDisplay acknowledged.");
+  // display.print("TEST R1");
+  // display2.print("TEST R2");
   // display.updateDisplay();
 
   //---------------------------------------------------
 
   // Initalize Servos
-  speed.attach(9);
-  rpm.attach(10);
+  // speed.attach(9);
+  // rpm.attach(10);
 
   //---------------------------------------------------
 
   // Initialize and join CAN
-  display.print("CAN INIT");
+  // display.print("CAN INIT");
   byte canInitResult = CAN.begin(MCP_ANY, CAN_100KBPS, MCP_8MHZ);
 
   if (canInitResult == CAN_OK)
@@ -58,36 +67,37 @@ void setup()
   {
 
     Serial.println("CAN Init Failed: CAN_FAILINIT");
-    display.print("CAN FAIL");
+    // display.print("CAN FAIL");
     while (1)
       ;
   }
   else if (canInitResult == CAN_FAILTX)
   {
     Serial.println("CAN Init Failed: CAN_FAILTX");
-    display.print("CAN FAIL");
+    // display.print("CAN FAIL");
     while (1)
       ;
   }
   else
   {
     Serial.println("CAN Init Failed: Unknown error");
-    display.print("CAN FAIL");
+    // display.print("CAN FAIL");
     while (1)
       ;
   }
 
-  CAN.init_Mask(0, 0, 0x7FF); // Set mask for filter 0 (standard 11-bit ID)
-  CAN.init_Filt(0, 0, 0x123); // Accept messages with CAN ID 0x123 (final ID TBD)
+  // CAN.init_Mask(0, 0, 0x7FF); // Set mask for filter 0 (standard 11-bit ID)
+  // CAN.init_Filt(0, 0, 0x123); // Accept messages with CAN ID 0x123 (final ID TBD)
 
   // Set the MCP2515 to normal mode to start receiving CAN messages
   CAN.setMode(MCP_NORMAL);
 
   // delay(2000);
-  display.print("INIT OK");
+  // display.print("INIT OK");
   delay(700);
-  display.print("NO MSG");
+  // display.print("NO MSG");
 }
+long unsigned int numberRX = 0;
 void loop()
 {
   // Handle Can RX
@@ -98,56 +108,51 @@ void loop()
   if (CAN_MSGAVAIL == CAN.checkReceive())
   {
 
-    CAN.readMsgBuf(&rxId, &len, rxBuf); // Read message
-    Serial.print(">ID: ");
+    while (CAN_MSGAVAIL == CAN.checkReceive())
+    {
+      CAN.readMsgBuf(&rxId, &len, rxBuf); // Read message
+      // Serial.print(">RX ID: ");
 
-    Serial.println(rxId, HEX);
-    display.print("RX" + rxId);
+      // Serial.println(rxId, HEX);
+      // display.print("RX" + rxId);
 
-    // for (int i = 0; i < len; i++) {
-    //   Serial.print(">Data: ");
-    //   Serial.println(rxBuf[i], HEX);
-    // }
-
-    Serial.println("-------------------------");
-
-    // Handle Command Response (TX)
-    //  if(rxBuf[1] == 0xA4){
-
-    //   byte sendMSG = CAN.sendMsgBuf(rxBuf[0],0,8,data);
-
-    //   // if(sendMSG == CAN_OK){
-    //   //   Serial.println("Message Sent Successfully!");
-    //   // } else {
-    //   //   Serial.println("Error Sending Message...");
-    //   // }
-    // }
+      // for (int i = 0; i < len; i++)
+      // {
+      //   Serial.print(">RX Data: ");
+      //   Serial.println(rxBuf[i], HEX);
+      // }
+      numberRX++;
+      if (numberRX % 10 == 0)
+        Serial.println(numberRX);
+    }
+    // Serial.println(" reached end of buffer -------------------------");
   }
-  byte error = CAN.checkError();
-  if (error == CAN_CTRLERROR)
-  {
-    Serial.println("CAN CTRL ERROR");
-    Serial.println(error, BIN);
-  }
-  else if (error == CAN_FAIL)
-  {
-    Serial.println("CAN FAIL");
-    Serial.println(error, BIN);
-  }
-  else if (error == CAN_FAILTX)
-  {
-    Serial.println("CAN FAIL TX");
-    Serial.println(error, BIN);
-  }
-  else if (error == CAN_OK)
-  {
-    Serial.println("CAN OK (explicit)");
-    Serial.println(error, BIN);
-  }
-  else
-  {
-    Serial.print("Check Error: ");
-    Serial.println(error, BIN);
-  }
-  delay(300);
+  // byte error = CAN.checkError();
+  // if (error == CAN_CTRLERROR)
+  // {
+  //   Serial.print("CAN CTRL ERROR -- Error Register: ");
+  //   Serial.println(error, BIN);
+  // }
+  // else if (error == CAN_FAIL)
+  // {
+  //   Serial.print("CAN FAIL -- Error Register: ");
+  //   Serial.println(error, BIN);
+  // }
+  // else if (error == CAN_FAILTX)
+  // {
+  //   Serial.print("CAN FAIL TX -- Error Register: ");
+  //   Serial.println(error, BIN);
+  // }
+  // else if (error == CAN_OK)
+  // {
+  //   Serial.print("CAN OK (explicit) -- Error Register ");
+  //   Serial.println(error, BIN);
+  // }
+  // else
+  // {
+  //   Serial.print("Check Error Register: ");
+  //   Serial.println(error, BIN);
+  // }
+  // delay(10);
+  // Serial.println("-------------------------");
 }
