a0ea647463d46f7843b730f6d2fdb6f34f28fdc8
karsteny <42128680+karsteny@users.noreply.github.com>
2024-11-16 14:36:04 -0700

    servos YAY

--- src/DevLib/Dash/Onboard/DashESP32/src/main.cpp
+++ src/DevLib/Dash/Onboard/DashESP32/src/main.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <SparkFun_Alphanumeric_Display.h>
-// #include <Servo.h>
+#include <ESP32Servo.h>
 #include <mcp_can.h>
 #include <SPI.h>
 #include <Adafruit_LEDBackpack.h>
@@ -11,13 +11,16 @@ const int CAN_CS_PIN = 5;
 
 // Devices
 HT16K33 display; // 14 segment
-// HT16K33 display2;
+HT16K33 display2;
 MCP_CAN CAN(CAN_CS_PIN); // CAN
-// Servo speed;
-// Servo rpm;
+Servo speed;
+Servo rpm;
 Adafruit_LEDBackpack ledMatrix = Adafruit_LEDBackpack();
 
 // Variables
+int lastSpeed = 0;
+int lastRPM = 0;
+int lastCVTTemp = 0;
 
 void setup()
 {
@@ -25,53 +28,55 @@ void setup()
   Serial.begin(115200);
   Serial.println("Dash test r8");
 
-
   // Join I2C
   Wire.begin();
-//   Wire.setWireTimeout(25000U, true);
+  //   Wire.setWireTimeout(25000U, true);
 
   //---------------------------------------------------
 
   // initalize 14 segment and check for display acknowledge
-  if (display.begin(0x70, 0x71, 0x72, 0x73) == false)
+  if (display.begin(0x70, 0x71) == false)
   {
     Serial.println("At least one 14 segment from row 1 did not acknowledge!");
     // while (1)
     //   ;
   }
-//   if (display2.begin(0x72, 0x73) == false)
-//   {
-//     Serial.println("At least one 14 segment from row 2 did not acknowledge!");
-//     // while (1)
-//     //   ;
-//   }
+  if (display2.begin(0x72, 0x73) == false)
+  {
+    Serial.println("At least one 14 segment from row 2 did not acknowledge!");
+    // while (1)
+    //   ;
+  }
   Serial.println("Display setup complete.");
-  display.print("TEST R1  AAAAAA");
-//   display2.print("TEST R2");
-  // display.updateDisplay();
+  display.print("TEST R1");
+  display2.print("TEST R2");
+  display.updateDisplay();
 
   //---------------------------------------------------
 
   // Initalize Servos
-//   speed.attach(5);
-//   rpm.attach(6);
+    speed.attach(32);
+    rpm.attach(33);
 
   //---------------------------------------------------
 
-  //Initialize LED Matrix
+  // Initialize LED Matrix
   if (ledMatrix.begin(0x74) == false)
   {
     Serial.println("LED Matrix Driver did not acknowledge!");
     // while (1)
     //   ;
   }
-  ledMatrix.displaybuffer[5] = 0b0100001000000001;
+  ledMatrix.displaybuffer[5] = 0b0000001100000000;
   ledMatrix.writeDisplay();
 
   //---------------------------------------------------
-
+  display.print("WELCOME");
+  display2.print("TO BAJA");
+  delay(2000);
   // Initialize and join CAN
-  display.print("CAN INIT");
+  // display.print("CAN INIT");
+  // display2.clear();
   byte canInitResult = CAN.begin(MCP_STDEXT, CAN_100KBPS, MCP_8MHZ);
 
   if (canInitResult == CAN_OK)
@@ -101,63 +106,104 @@ void setup()
       ;
   }
 
-  CAN.init_Mask(0, 1, 0x00000001); 
-  CAN.init_Filt(0, 1, 0x00000001);
-  CAN.init_Filt(1, 1, 0x00000001);
-  
+  // CAN.init_Mask(0, 1, 0x00000001);
+  // CAN.init_Filt(0, 1, 0x00000001);
+  // CAN.init_Filt(1, 1, 0x00000001);
 
-  CAN.init_Mask(1, 1, 0x00000001); 
-  CAN.init_Filt(2, 1, 0x00000001);
-  CAN.init_Filt(3, 1, 0x00000001);
-  CAN.init_Filt(4, 1, 0x00000001);
-  CAN.init_Filt(5, 1, 0x00000001);
-
+  // CAN.init_Mask(1, 1, 0x00000001);
+  // CAN.init_Filt(2, 1, 0x00000001);
+  // CAN.init_Filt(3, 1, 0x00000001);
+  // CAN.init_Filt(4, 1, 0x00000001);
+  // CAN.init_Filt(5, 1, 0x00000001);
 
   // Set the MCP2515 to normal mode to start receiving CAN messages
   CAN.setMode(MCP_NORMAL);
 
   // delay(2000);
+  display2.clear();
   display.print("INIT OK");
-  delay(700);
-  display.print("NO MSG");
-//   display2.print("WAITING");
   Serial.println("Init Ok!");
+  delay(1500);
+  // display.print("WAITING");
+  // display2.print("FOR CAN");
+
+  
 }
 
+int num = 0;
+int underscore = 0;
 void loop()
 {
   // Handle Can RX
   unsigned long rxId;
   unsigned char len = 0;
   unsigned char rxBuf[8];
-
-  if (CAN_MSGAVAIL == CAN.checkReceive())
+  while (CAN_MSGAVAIL == CAN.checkReceive())
   {
+    CAN.readMsgBuf(&rxId, &len, rxBuf); // Read message
+    // Serial.print(">RX ID: ");
 
-    while (CAN_MSGAVAIL == CAN.checkReceive())
+    // Serial.println(rxId, HEX);
+
+    // display.print(rxId);
+    // display2.print(rxId);
+    switch (rxBuf[1])
     {
-      CAN.readMsgBuf(&rxId, &len, rxBuf); // Read message
-      Serial.print(">RX ID: ");
-
-      Serial.println(rxId, HEX);
-    //   if(rxId > 1){
-        display.print(rxId);    
-    //   }
-      
-
-    //   display2.print(rxId);
-
-      // for (int i = 0; i < len; i++)
-      // {
-      //   Serial.print(">RX Data: ");
-      // Serial.println(rxBuf[i], HEX);
-      // }
+    case 0x01:
+      // Speed
+      // TODO: if multithreading, lock
+      lastSpeed = rxBuf[2];
+      break;
+    case 0x02:
+      // RPM
+      // TODO: if multithreading, lock
+      lastRPM = rxBuf[2];
+      break;
+    case 0x03:
+      // CVT Temp
+      // TODO: if multithreading, lock
+      lastCVTTemp = rxBuf[2];
+      break;
     }
-    // Serial.println(" reached end of buffer -------------------------");
+  }
+  num++;
+  //really should add this to a method cause it's just taking up room
+  if(num%6000==0){
+    underscore++;
+    if(underscore==1) display.print("CAN_");
+    else if(underscore==2) display.print("CAN _");
+    else if(underscore==3) display.print("CAN  _");
+    else if(underscore==4) display.print("CAN   _");
+    else if(underscore==5) display.print("CAN  _");
+    else if(underscore==6) {
+      display.print("CAN _");
+      underscore = 0;
+    }
   }
 
-//   if (Wire.getWireTimeoutFlag())
-//   {
-//     Serial.println("WIRE TIMEOUT FLAG SET!!!");
-//   }
+  //test led drive
+  if(num%20000==0){
+    if(num%40000 == 0){
+      ledMatrix.displaybuffer[5] = 0b0000001000000000;
+      ledMatrix.writeDisplay();
+      speed.write(20);
+      rpm.write(120);
+    }else{
+      ledMatrix.displaybuffer[5] = 0b0000000100000000;
+      ledMatrix.writeDisplay();
+      speed.write(120);
+      rpm.write(20);
+    }
+  }
+
+  if(num%2000==0){
+    unsigned long currentSeconds = millis()/1000;
+    unsigned long currentMinutes = currentSeconds/60;
+    unsigned long currentHours = currentSeconds/3600;
+    display2.printf("%02u%02u  %02u", currentHours, currentMinutes%60, currentSeconds%60);  
+    display2.colonOn();
+  }
+  
+  
+
 }
