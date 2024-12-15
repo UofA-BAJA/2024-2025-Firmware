8c8b1f0e3086963af9e4df0f246be6f09ecc5f04
BAJA ELECTRICAL <uabaja@gmail.com>
2024-12-14 22:24:25 -0700

    finally fixed the stupidest bug ever. put the correct variable names in the function parameters!

--- src/CarComputer/include/CarContainer.h
+++ src/CarComputer/include/CarContainer.h
@@ -6,11 +6,32 @@
 #include "DataStorage.h"
 #include "Coms.h"
 
+
+#include "procedures/IMUProcedure.cpp"
+#include "procedures/DashProcedure.cpp"
+#include "procedures/TemperatureProcedure.cpp"
+
+#include "IMUSubsystem.h"
+#include "DashSubsystem.h"
+#include "TemperatureSubsystem.h"
+
 // class Car;
 
 class CarContainer{
     public:
         CarContainer(ProcedureScheduler* procedureScheduler, CANDispatcher* canDispatcher, DataStorage* dataStorage, Coms* coms);
+    private:
+        IMUSubsystem* imuSubsystem;
+        IMUProcedure* imuProcedure;
+
+        DashSubsystem* dashSubsystem;
+        DashProcedure* dashProcedure;
+
+        TemperatureSubsystem* temperatureSubsystem;
+        TemperatureProcedure* temperatureProcedure;
+
+
+
 };
 
 #endif
