3a3d8c33475d26b529b4cb399268923b3aea0ed5
bigpapa132525 <159649867+bigpapa132525@users.noreply.github.com>
2025-01-19 20:47:12 -0700

    tach updates

---
+++ hardware/KICAD Projects/BAJE SAE/Tachometer/randomecode.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(void){
+
+printf("your mom gay");
+
+
+
+    return 0;
+}
---
+++ hardware/KICAD Projects/BAJE SAE/Tachometer/tachometer.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+void setup(){
+    Serial.begin(6900);
+}
+void loop(){
+//  int sensorvalue = analogRead(A0);
+
+//  float voltage =  sensorvalue *  ( 5/ 1023.0);
+println
+
+    return 0;
+}
